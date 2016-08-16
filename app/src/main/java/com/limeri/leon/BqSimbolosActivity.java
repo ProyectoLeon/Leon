971e9c2a2097ee5b4468d5af8bcec6170df6c61a
mauiglesias <mauro.iglesias@wirsolut.com>
2016-08-16 02:50:05 -0300

    Ajuste en Busqueda de simbolos (especialmente logica del boton cancelar)

--- app/src/main/java/com/limeri/leon/BqSimbolosActivity.java
+++ app/src/main/java/com/limeri/leon/BqSimbolosActivity.java
@@ -1,9 +1,13 @@
 package com.limeri.leon;
 
-import android.os.Bundle;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.ImageView;
@@ -21,15 +25,19 @@ import org.json.JSONObject;
 public class BqSimbolosActivity extends AppCompatActivity {
 
 
+    private Button respuestaSi;
+    private Button respuestaNo;
     private String pregunta;
     private String respuesta;
     private ImageView muestra;
     private ImageView imagesimbolo;
     private String respuestaSeleccionada = "";
     private int nivel = 0;
+    private int cantIncorrectas = 0;
+    private int cantCorrectas = 0;
     private String jsonString;
     private int puntaje;
-    private long tiempo_detenido;
+    private long tiempo_ejecutado = 0;
     private Chronometer crono;
 
 
@@ -38,9 +46,61 @@ public class BqSimbolosActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bq_simbolos);
 
-        //BORRAR?   palabra = (TextView) findViewById(R.id.palabra);
+        //CONFIGURACION DEL BOTON DE CANCELAR JUEGO - Comienza aquí
+        final ViewGroup actionBarLayout = (ViewGroup) getLayoutInflater().inflate(
+                R.layout.action_bar,
+                null);
 
-        Navegacion.agregarMenuJuego(this);
+        getSupportActionBar().setCustomView(actionBarLayout);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+
+        Button boton = (Button) getSupportActionBar().getCustomView().findViewById(R.id.boton_actionbar);
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                crono.stop();
+                tiempo_ejecutado = tiempo_ejecutado + SystemClock.elapsedRealtime() - crono.getBase();
+                AlertDialog.Builder builder = new AlertDialog.Builder(BqSimbolosActivity.this);
+
+                LayoutInflater inflater = getLayoutInflater();
+                View dialogView = inflater.inflate(R.layout.alert_dialog, null);
+
+                builder.setView(dialogView);
+
+                Button btn_positive = (Button) dialogView.findViewById(R.id.dialog_positive_btn);
+                Button btn_negative = (Button) dialogView.findViewById(R.id.dialog_negative_btn);
+                Button btn_neutral = (Button) dialogView.findViewById(R.id.dialog_neutral_btn);
+
+                final AlertDialog dialog = builder.create();
+
+                btn_positive.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        AdministradorJuegos.getInstance().guardarJuego(BqSimbolosActivity.this);
+                    }
+                });
+
+                btn_negative.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        AdministradorJuegos.getInstance().cancelarJuego(BqSimbolosActivity.this);
+                    }
+                });
+
+                btn_neutral.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        crono = (Chronometer) findViewById(R.id.cronometro);
+                        crono.setBase(SystemClock.elapsedRealtime());
+                        crono.start();
+                        dialog.cancel();
+                    }
+                });
+
+                // Display the custom alert dialog on interface
+                dialog.show();
+            }
+        });
 
         //Llamo una funcion que se encarga de leer el archivo JSON
         crono = (Chronometer) findViewById(R.id.cronometro);
@@ -55,7 +115,6 @@ public class BqSimbolosActivity extends AppCompatActivity {
 
         if (nivel == 0) {
             jsonString = JSONLoader.loadJSON(getResources().openRawResource(R.raw.busquedasimbolos));
-        nivel++;
         }
 
         try {
@@ -64,18 +123,19 @@ public class BqSimbolosActivity extends AppCompatActivity {
             //Get the instance of JSONArray that contains JSONObjects
             JSONArray jsonArray = jsonRootObject.getJSONArray("simbolos");
 
-            if (nivel > jsonArray.length()){
+            if (nivel > jsonArray.length()) {
                 guardar();
             }
             JSONObject jsonObject = jsonArray.getJSONObject(nivel);
+            nivel++;
             pregunta = (jsonObject.getString("pregunta").toString());
             respuesta = (jsonObject.optString("respuesta0").toString());
             muestra = (ImageView) findViewById(R.id.muestra);
             imagesimbolo = (ImageView) findViewById(R.id.imageVSimbolos);
-            String nivelSimbolo = "bs"+nivel;
-            String nivelMuestra = "m"+nivel;
-            int simb = getResources().getIdentifier(nivelSimbolo,"drawable",getPackageName());
-            int muest = getResources().getIdentifier(nivelMuestra, "drawable",getPackageName());
+            String nivelSimbolo = "bs" + nivel;
+            final String nivelMuestra = "m" + nivel;
+            int simb = getResources().getIdentifier(nivelSimbolo, "drawable", getPackageName());
+            int muest = getResources().getIdentifier(nivelMuestra, "drawable", getPackageName());
             imagesimbolo.setImageResource(simb);
             muestra.setImageResource(muest);
 
@@ -84,48 +144,49 @@ public class BqSimbolosActivity extends AppCompatActivity {
                 respuestaSi.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        long elapsedMillis = SystemClock.elapsedRealtime() - crono.getBase() - tiempo_detenido;
-                        if (elapsedMillis/1000 < 120) {
-                            if (respuesta.equals("true")){
+                        long elapsedMillis = SystemClock.elapsedRealtime() - crono.getBase() + tiempo_ejecutado;
+                        if (elapsedMillis / 1000 < 120) {
+                            if (respuesta.equals("true") & (nivel >4)) {
                                 sumarPuntos(1);
                             } else {
                                 sumarPuntos(-1);
                             }
-                            nivel++;
+
                             try {
                                 leerJson();
                             } catch (Exception ex) {
                                 guardar();
                             }
-                        }else{
+                        } else {
                             guardar();
                         }
 
-                    }});
+                    }
+                });
             }
             Button respuestaNo = (Button) findViewById(R.id.buttonNo);
             if (respuestaNo != null) {
                 respuestaNo.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        long elapsedMillis = SystemClock.elapsedRealtime() - crono.getBase() - tiempo_detenido;
-                        if (elapsedMillis/1000 < 120) {
-                            if (respuesta.equals("false")) {
+                        long elapsedMillis = SystemClock.elapsedRealtime() - crono.getBase() + tiempo_ejecutado;
+                        if (elapsedMillis / 1000 < 120) {
+                            if (respuesta.equals("false") & (nivel>4)) {
                                 sumarPuntos(1);
                             } else {
                                 sumarPuntos(-1);
                             }
-                            nivel++;
                             try {
                                 leerJson();
                             } catch (Exception ex) {
                                 guardar();
                             }
-                        }else {
+                        } else {
                             guardar();
                         }
 
-                    }});
+                    }
+                });
             }
 
 
@@ -136,12 +197,17 @@ public class BqSimbolosActivity extends AppCompatActivity {
 
     }
 
+
     private void sumarPuntos(Integer puntos) {
         Juego juego = Paciente.getSelectedPaciente().getEvaluacionActual().getJuegoActual();
         juego.setPuntosJuego(juego.getPuntosJuego() + puntos);
     }
 
     private void guardar() {
+        Juego juego = Paciente.getSelectedPaciente().getEvaluacionActual().getJuegoActual();
+        if (juego.getPuntosJuego() < 0) {
+            juego.setPuntosJuego(0);
+        }
         AdministradorJuegos.getInstance().guardarJuego(this);
     }
 }
